50081700fcf5a9e70eb5b84cffb39de611dc8f7d
Slava Leleka <v.leleka@adguard.com>
2023-07-16 02:44:21 +0300

    one more update

--- docs/readme.txt
+++
@@ -1,3 +0,0 @@
-# Short url for the iOS shortcut
-
-It is needed to host a simple redirect for the short url on gh-pages.
--- docs/redirect.js
+++
@@ -1,9 +0,0 @@
-const TARGET_URL = 'http://example.com';
-
-const redirect = () => {
-    const { location } = window;
-    debugger;
-    location.replace(TARGET_URL);
-};
-
-redirect();
